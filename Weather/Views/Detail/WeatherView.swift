2fba1f127b0bbd195cace7e586fd37cdb7d257a3
Ioanna Karageorgou <i.karageorgou@withplum.com>
2023-07-13 14:50:00 +0300

    Add custom navigation bar

---
+++ Weather/Views/Detail/WeatherView.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherView.swift
+//  Weather
+//
+//  Created by Ioanna Karageorgou on 13/7/23.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        ZStack {
+            // MARK: Background
+            Color.background
+                .ignoresSafeArea()
+        }
+        .overlay {
+            // MARK: Navigation Bar
+            NavigationBar()
+        }
+        .navigationBarHidden(true)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
